87c9d1c770d00d5ec8bbf735e82571a5500e31e7
Michiel de Jong <michiel@unhosted.org>
2014-10-07 16:06:38 +0100

    add approve-user loop

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,4 +6,7 @@ ssh root@$1 sh ./onServer.sh
 cd ../orchestration/per-server/$1/sites/
 for i in * ; do
   echo "setting up site $i as `cat $i` on $1";
+  ssh root@$1 mkdir -p /data/per-user/$i/
+  scp ../../../TLS/approved-certs/$i.pem root@$1:/data/per-user/$i/combined.pem
+  ssh root@$1 sudo sh /data/infrastructure/scripts/approve-user.sh $i `cat $i`
 done
