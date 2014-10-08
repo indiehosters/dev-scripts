4cffc3ab3eb12e1ef75fb0177bf4878899013007
Michiel de Jong <michiel@unhosted.org>
2014-10-08 15:31:58 +0100

    fix script for creating ../orchestration/TLS/combined/*.pem

--- deploy/check-cert.sh
+++ deploy/check-cert.sh
@@ -15,7 +15,7 @@ openssl x509 -text -in ../orchestration/TLS/cert/$1.cert | head -50 | grep -v ^\
 #echo Some information about key ../orchestration/TLS/key/$1.key:
 #openssl rsa -text -in ../orchestration/TLS/key/$1.key
 
-cat ../orchestration/TLS/cert/$1.cert ../orchestration/TLS/chain/$2.pem ../orchestration/TLS/key/$1.key > ../orchestration/TLS/combined/$1.pem
+cat ../orchestration/TLS/cert/$1.cert ../orchestration/TLS/chain/$CA.pem ../orchestration/TLS/key/$1.key > ../orchestration/TLS/combined/$1.pem
 
 echo Running a test server on port 4433 on this server now \(please use your browser to check\):
 openssl s_server -cert ../orchestration/TLS/combined/$1.pem -www
