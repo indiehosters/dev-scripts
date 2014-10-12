6a72afd7f0413cf7a92434197535f3c054c215be
Michiel de Jong <michiel@unhosted.org>
2014-10-12 17:59:21 +0100

    support infrastructure branches other than master

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+if [ $# -eq 2 ]; then
+  BRANCH=$2
+else
+  BRANCH="master"
+fi
+echo "Infrastructure branch is $BRANCH"
+
 chmod -R go-w ../orchestration/deploy-keys
 scp -r ../orchestration/deploy-keys root@$1:.ssh
 scp ./deploy/onServer.sh root@$1:
-ssh root@$1 sh ./onServer.sh
+ssh root@$1 sh ./onServer.sh $BRANCH
 cd ../orchestration/per-server/$1/sites/
 for i in * ; do
   echo "setting up site $i as `cat $i` on $1";
--- deploy/onServer.sh
+++ deploy/onServer.sh
@@ -3,4 +3,7 @@ mkdir /data
 cd /data
 git clone https://github.com/indiehosters/infrastructure.git
 cd infrastructure
+echo Checking out $1 branch of the IndieHosters infrastructure:
+git checkout $1
+echo Running the server setup script:
 sh scripts/setup.sh
