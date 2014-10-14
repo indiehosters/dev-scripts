22c027d41b8afdf931b559a1f346269f800c672e
Michiel de Jong <michiel@unhosted.org>
2014-10-14 15:46:13 +0100

    mkdir -p /var/lib/coreos-install/

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,6 +11,7 @@ if [ -f ../orchestration/deploy-keys/authorized_keys ]; then
   scp -r ../orchestration/deploy-keys root@$1:.ssh
 fi
 scp ./deploy/onServer.sh root@$1:
+ssh root@$1 mkdir -p /var/lib/coreos-install/
 scp ../infrastructure/cloud-config root@$1:/var/lib/coreos-install/user_data
 ssh root@$1 sh ./onServer.sh $BRANCH
 cd ../orchestration/per-server/$1/sites/
